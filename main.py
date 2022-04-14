3d89ee8d9d5e3a2a7cf47a5e94f0249c51939cf6
Mrinal Jain <2mrinaljain@gmail.com>
2022-04-14 17:40:13 +0530

    sentence similarity detection

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import twitter_search as ts
 import sentiment
-import json
+import requests
+
+SENTENCE_SIMILARITY = "https://api-inference.huggingface.co/models/sentence-transformers/all-MiniLM-L6-v2"
+headers = {"Authorization": bearer}
+
+def query(payload):
+	response = requests.post(SENTENCE_SIMILARITY, headers=headers, json=payload)
+	return response.json()
 
 data = ts.get_tweets()['data']
-#with open('tweets.txt', 'r') as f:
-#	data = json.load(f)['data']
 pos, neg = sentiment.get_results(data)
-pos = ts.filter_uc(pos)
-for i in pos:
-	print(i)
+q_params = {'inputs': {
+	'source_sentence': 'The service was excellent. The person was polite and professional',
+	'sentences': [i['text'] for i in pos]
+}}
+similarity_score = query(q_params)
+
+for i in range(len(pos)):
+	if similarity_score[i] >= 0.3:
+		print(pos[i])
--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ python3 -m venv .env
 source .env/bin/activate
 pip3 install "transformers[sentencepiece]"
 pip3 install torch
+pip3 install -U sentence-transformers
